748aa8a5fb1ea51838e09ab803e84aa107e16307
Prathamesh Mutkure <pmutkure009@gmail.com>
2019-04-06 17:23:31 +0530

    Changed FIle Structure

--- Projects/Simple/SalaryCalculator/main.c___jb_old___/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeCache.txt/cmake-build-debug/CMakeFiles/3.13.2/CompilerIdC/tmp/link.txt/workspace.xml___jb_old___/src-out/classToSubclasses.tab_i.len/tmp/CMakeDirectoryInformation.cmake
+++
@@ -1,3 +0,0 @@
-/home/prathamesh/Desktop/C-Language/MyWorkC/GaussSeidal/cmake-build-debug/CMakeFiles/rebuild_cache.dir
-/home/prathamesh/Desktop/C-Language/MyWorkC/GaussSeidal/cmake-build-debug/CMakeFiles/GaussSeidal.dir
-/home/prathamesh/Desktop/C-Language/MyWorkC/GaussSeidal/cmake-build-debug/CMakeFiles/edit_cache.dir
